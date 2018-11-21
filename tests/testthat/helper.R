235703b4e150e0f5e329d05f089ec2a70356c760
Nick Pelikan <nick.pelikan@yougov.com>
2018-11-20 21:02:01 -0700

    advocate mocks

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -13,4 +13,9 @@ library(httptest)
 #     httr::GET("https://api.oyez.org/cases?per_page=0&filter=term:2014")
 #     # transcript for 2014/13-1175
 #     httr::GET("https://api.oyez.org/case_media/oral_argument_audio/23314")
+#     # advocates for 2014/13-1175
+#     httr::GET("https://api.oyez.org/people/e_joshua_rosenkranz/cases")
+#     httr::GET("https://api.oyez.org/people/michael_r_dreeben/cases")
+#     httr::GET("https://api.oyez.org/people/thomas_c_goldstein/cases")
 # }, path = "tests/testthat")
+
