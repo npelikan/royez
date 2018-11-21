c227df0fc4e5910e4d498a7c5e0d87510e6ec03e
nick.pelikan <nick.pelikan@yougov.com>
2018-11-21 15:27:38 -0700

    caselist parsing

--- R/oyez_case.R
+++ R/oyez_case.R
@@ -24,3 +24,56 @@ oyez_case <- function(term, docket_number){
     class(res) <- "OyezCase"
     res
 }
+
+
+
+
+#' Get multiple oyez cases
+#'
+#'
+#'
+#' @param x a data.frame or list
+#' @param ...
+#'
+#' @return an OyezCaseList object
+#' @export
+oyez_cases <- function(x, ...){
+    UseMethod('oyez_cases')
+}
+
+
+#' @rdname oyez_cases
+#' @importFrom purrr pmap modify_if
+#' @export
+oyez_cases.data.frame <- function(x, ...){
+    if (any(!c("term", "docket_number") %in% names(x))) {
+        stop('data.frame inputs to `oyez_cases()` must contain columns term and docket_number',
+             call. = F)
+    }
+    # turns factors to characters
+    x <- purrr::modify_if(x, is.factor, as.character)
+    out <- purrr::pmap(x, oyez_case)
+    class(out) <- "OyezCaseList"
+    out
+}
+
+
+#' @rdname oyez_cases
+#' @importFrom purrr map
+#' @export
+oyez_cases.list <- function(x, ...){
+
+    get_cases_ <- function(i){
+        if (any(!c("term", "docket_number") %in% names(i))) {
+            stop('list inputs to `oyez_cases()` must be a list of lists \n
+                 with all sublists containing fields term and docket_number',
+                 call. = F)
+        }
+
+        oyez_case(term = i$term, docket_number = i$docket_number)
+    }
+
+    out <- purrr::map(x, get_cases_)
+    class(out) <- "OyezCaseList"
+    out
+}
--- tests/testthat/test-oyez_case.R
+++ tests/testthat/test-oyez_case.R
@@ -10,5 +10,94 @@ with_mock_api({
     test_that("bad searches invoke a stop", {
         expect_error(oyez_case(term = '2014', docket_number = 'barf'))
     })
+
+
+    ## mutliple cases!
+
+    ## data.frame ingest
+    good_df <- data.frame(
+        term = c("2014", "2014", "2000"),
+        docket_number = c("13-1175", "13-1499", "00-949")
+    )
+
+    test_that("dataframe parse creates expected object", {
+        df_out <- oyez_cases(good_df)
+        expect_s3_class(df_out, "OyezCaseList")
+        expect_length(df_out, 3)
+    })
+
+    good_df_nofactor <- data.frame(
+        term = c("2014", "2014", "2000"),
+        docket_number = c("13-1175", "13-1499", "00-949"),
+        stringsAsFactors = F
+    )
+
+    test_that("dataframe parse creates expected object with no factors", {
+        df_out <- oyez_cases(good_df_nofactor)
+        expect_s3_class(df_out, "OyezCaseList")
+        expect_length(df_out, 3)
+    })
+
+    test_that("dataframe parse errors on bad input", {
+        # stop error
+        expect_error(oyez_cases(data.frame(term = c("2012"))),
+                     regexp = 'data.frame inputs to `oyez_cases()` must contain columns term and docket_number',
+                     fixed = TRUE)
+        # no such case error
+        expect_error(
+            oyez_cases(data.frame(
+                term = c("2014", "2000"),
+                docket_number = c("barf", "00-949")))
+        )
+    })
+
+    ## list ingest
+    good_list <- list(
+        list(
+            term = "2014",
+            docket_number = "13-1175"
+        ),
+        list(
+            term = "2014",
+            docket_number = "13-1499"
+        ),
+        list(
+            term = "2000",
+            docket_number = "00-949"
+        )
+    )
+
+    test_that("list parse creates expected objects", {
+        ls_out <- oyez_cases(good_list)
+        expect_s3_class(ls_out, "OyezCaseList")
+        expect_length(ls_out, 3)
+    })
+
+    test_that("list parse errors on bad input", {
+        # input error
+        expect_error(
+            oyez_cases(list(
+                list(
+                    term = "2014",
+                    docket_number = "13-1175"
+                ),
+                list(
+                    term = "2014"
+                )
+                ))
+        )
+        expect_error(
+            oyez_cases(list(
+                list(
+                    term = "2014",
+                    docket_number = "13-1175"
+                ),
+                list(
+                    term = "2014",
+                    docket_number = 'barf'
+                )
+            ))
+        )
+    })
 })
 
