39bdc5661e14196e7d240c5c891bb1ee7d9528f8
nick.pelikan <nick.pelikan@yougov.com>
2018-11-27 21:15:25 -0600

    initial test for as.data.frame.OyezTranscript

--- R/OyezTranscript_methods.R
+++ R/OyezTranscript_methods.R
@@ -1,6 +1,27 @@
+
 #' @importFrom purrr map map_dfr pmap_dfr
 as.data.frame.OyezTranscript <- function(x){
-    pt <- purrr::map_dfr(t[['sections']], function(sec){
+    pt <- purrr::map_dfr(x[['sections']], function(sec){
 
+        purrr::map2_dfr(sec, names(sec),
+                        unpack_turns_)
     })
+    pt
+}
+
+#' @importFrom map2_dfr map_chr
+unpack_turns_ <- function(turns, nm){
+    td <- purrr::map2_dfr(turns, 1:length(turns), function(t, n){
+        sn <- t[['speaker']][['name']]
+
+        tx <- purrr::map_chr(t[['text_blocks']], 'text')
+
+        data.frame(sequence = n,
+                   speaker_name = sn,
+                   text = tx,
+                   stringsAsFactors = F)
+    })
+
+    td$section <- nm
+    td
 }
---
+++ tests/testthat/test-OyezTranscript_methods.R
@@ -0,0 +1,13 @@
+context("test-OyezTranscript_methods.R")
+
+with_mock_api({
+    test_that("data.frame of correct names is generated", {
+        a <- oyez_case(term = 2014, docket_number='13-1175')
+        t <- transcript(a)
+        df <- as.data.frame(t)
+        expect_is(df, 'data.frame')
+        expect_named(df, c('sequence', 'speaker_name', 'text', 'case_name', 'section'),
+                     ignore.order = T)
+    })
+})
+
