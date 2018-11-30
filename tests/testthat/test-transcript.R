719f21e6cb9705ae7fd65377fa9ba717e82c2984
Nick Pelikan <nick.pelikan@yougov.com>
2018-11-29 18:45:04 -0700

    even more test coverage

--- tests/testthat/test-transcript.R
+++ tests/testthat/test-transcript.R
@@ -2,6 +2,8 @@ context("test-transcript.R")
 
 
 with_mock_api({
+
+    ## single case
     c <- oyez_case(term = 2014, docket_number='13-1175')
     trans <- transcript(c)
 
@@ -21,5 +23,22 @@ with_mock_api({
 
     test_that("correct number of turns parsed", {
         expect_length(trans$sections[[1]], 348)
+    })
+
+    ## multiple cases
+    good_df <- data.frame(
+        term = c("2014", "2000"),
+        docket_number = c("13-1175", "00-949")
+    )
+    cl <- oyez_cases(good_df)
+    tl <- transcript(cl)
+
+    test_that("correct object types returned", {
+        expect_s3_class(tl, "OyezTranscriptList")
+        expect_s3_class(tl[[1]], "OyezTranscript")
+    })
+
+    test_that("correct number of items returned", {
+        expect_length(tl, 2)
     })
 })
